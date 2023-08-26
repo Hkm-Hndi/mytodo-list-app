a2a5206053071c0d5132999054ae1bf7e91e9796
Hkm-Hndi <hakam.alhendi@gmail.com>
2023-08-26 21:45:18 +0300

    Implemented basic logging functionality to improve trace and debugging

--- src/main/java/com/hndi/mytodo/controller/CategoryController.java
+++ src/main/java/com/hndi/mytodo/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.hndi.mytodo.controller;
 
 import com.hndi.mytodo.model.Category;
 import com.hndi.mytodo.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,32 +12,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/todosCategories")
 public class CategoryController {
-
+    private final Logger logger= LoggerFactory.getLogger(CategoryController.class);
     @Autowired
     private CategoryService categoryService;
 
     @GetMapping
     public List<Category> getAllCategories() {
+        logger.trace("getting all categories");
         return categoryService.getAllCategories();
     }
 
     @GetMapping("/{id}")
     public Category getCategory(@PathVariable("id") String id) {
+        logger.trace("getting category "+ id);
         return categoryService.getCategory(id);
     }
 
     @PostMapping
     public void addCategory(@RequestBody Category category) {
+        logger.trace("adding category "+ category.getName());
         categoryService.addCategory(category);
     }
 
     @PutMapping("/{id}")
     public Category updateCategory(@PathVariable("id") String id, @RequestBody Category category) {
+        logger.trace("updating category "+ id);
         return categoryService.updateCategory(id, category);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable("id") String id) {
+        logger.trace("deleting category "+ id);
         categoryService.deleteCategory(id);
     }
 }
--- src/main/java/com/hndi/mytodo/controller/ItemController.java
+++ src/main/java/com/hndi/mytodo/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.hndi.mytodo.controller;
 
 import com.hndi.mytodo.model.Item;
 import com.hndi.mytodo.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +13,13 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/api/v1/todos")
 public class ItemController {
-
+    private final Logger logger= LoggerFactory.getLogger(ItemController.class);
     @Autowired
     private ItemService itemService;
 
     @GetMapping
     private List<Item> getItemsByCategory(@RequestParam(name="category", required = false) String categoryId){
+        logger.trace("getting all items of category "+ categoryId);
         if (Objects.nonNull(categoryId))
             return itemService.getItemsByCategory(categoryId);
         else
@@ -26,21 +29,25 @@ public class ItemController {
 
     @GetMapping("{id}")
     public Item getItem(@PathVariable("id") String id) {
+        logger.trace("getting item "+ id);
         return itemService.getItem(id);
     }
 
     @PostMapping
     private void addItem(@RequestBody Item item) {
+        logger.trace("adding item "+ item.getName());
         itemService.addItem(item);
     }
 
     @PutMapping("{id}")
     private Item updateItem(@PathVariable("id") String id, @RequestBody Item item) {
+        logger.trace("updaing item "+ id);
         return itemService.updateItem(id, item);
     }
 
     @DeleteMapping("{id}")
     private void deleteItem(@PathVariable("id") String id) {
+        logger.trace("deleting item "+ id);
         itemService.deleteItem(id);
     }
 }
